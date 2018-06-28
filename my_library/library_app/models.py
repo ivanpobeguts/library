af66450c2749e2b4adee7caecf6e0a55f985fcca
Pobeguts Ivan <Ivan.Pobeguts@aton.ru>
2018-06-28 19:01:49 +0300

    add login and book list

--- my_library/library_app/models.py
+++ my_library/library_app/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Book(models.Model):
     name = models.CharField(max_length=50)
     author = models.CharField(max_length=50)
-    users = models.ManyToManyField(User)
+    users = models.ManyToManyField(User, blank=True)
+    stars = models.IntegerField(validators=[MinValueValidator(0),
+                                            MaxValueValidator(5)],
+                                blank=True,
+                                null=True
+                                )
     # registered = models.DateTimeField
--- my_library/library_app/views.py
+++ my_library/library_app/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.template import RequestContext
 
-# Create your views here.
+from .models import Book
+from django.shortcuts import render, redirect
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+
+
+@login_required
+def index(request):
+    books_list = Book.objects.all()
+    context = {'books_list': books_list}
+    return render(request, 'library/index.html', context)
+
+
+def login(request):
+    if request.method == "GET":
+        return render(request, 'library/login.html')
+    elif request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None and user.is_active:
+            auth.login(request, user)
+            return redirect('index')
+        else:
+            pass
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect("/library/logout/")
